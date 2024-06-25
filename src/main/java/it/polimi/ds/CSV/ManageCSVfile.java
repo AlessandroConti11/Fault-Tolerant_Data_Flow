20f7b82fab23d6501a21200233f12e88fd74bd41
alessandro-conti <alessandro11.conti@mail.polimi.it>
2024-06-25 20:21:08 +0200

    master_DAG: 1st implementation of DAG + Operation to execute ar implemented

---
+++ src/main/java/it/polimi/ds/CSV/ManageCSVfile.java
@@ -0,0 +1,236 @@
+package it.polimi.ds.CSV;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import it.polimi.ds.function.FunctionName;
+import it.polimi.ds.function.OperatorName;
+import org.javatuples.Pair;
+import org.javatuples.Triplet;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The ManageCSVfile class provides methods for reading input tuples and
+ * operations from a CSV file.
+ */
+public class ManageCSVfile {
+    /**
+     * Reads the number of partition required from the dataflow.
+     *
+     * @param csvFile the input file.
+     * @return the number of partition.
+     */
+    public static Integer readCSVpartition(File csvFile) {
+        if (csvFile.length() == 0) {
+            return 0;
+        }
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            String[] records = reader.readNext();
+
+            return Integer.parseInt(records[0].split(";")[0]);
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * Reads the input tuples from CSV file.
+     *
+     * @param csvFile the CSV input file.
+     * @return the input tuple.
+     */
+    public static List<Pair<Integer, Integer>> readCSVinput(File csvFile) {
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            List<String[]> records = reader.readAll();
+
+            // reads all input tuple
+            for (int i = 1; i < records.size(); i++) {
+                result.add(new Pair<>(Integer.parseInt(records.get(i)[0].split(";")[0]),
+                        Integer.parseInt(records.get(i)[0].split(";")[1])));
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * Reads the input tuples from CSV file.
+     *
+     * @param csvFile the CSV input file.
+     * @return the input tuple.
+     */
+    public static List<Pair<Integer, Integer>> readCSVresult(File csvFile) {
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            List<String[]> records = reader.readAll();
+
+            // reads all input tuple
+            for (int i = 0; i < records.size(); i++) {
+                result.add(new Pair<>(Integer.parseInt(records.get(i)[0].split(";")[0]),
+                        Integer.parseInt(records.get(i)[0].split(";")[1])));
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * Reads the input tuples from CSV file.
+     *
+     * @param csvFile the CSV input file.
+     * @return the input tuple.
+     */
+    public static List<Pair<Integer, Integer>> readCSVinput(File csvFile, int start, int end) {
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            List<String[]> records = reader.readAll();
+
+            // reads all input tuple
+            for (int i = start - 1; i < end; i++) {
+                result.add(new Pair<>(Integer.parseInt(records.get(i)[0].split(";")[0]),
+                        Integer.parseInt(records.get(i)[0].split(";")[1])));
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * Reads the input operation from CSV file.
+     *
+     * @param csvFile the CSV input file.
+     * @return the operation to be carried out.
+     */
+    public static List<Triplet<OperatorName, FunctionName, Integer>> readCSVoperation(File csvFile) {
+        List<Triplet<OperatorName, FunctionName, Integer>> result = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            List<String[]> records = reader.readAll();
+
+            // reads all operation to perform - start from the last tuple input
+            for (int i = 0; i < records.size(); i++) {
+                result.add(new Triplet<>(OperatorName.getsEnumerationValue(records.get(i)[0].split(";")[0]),
+                        FunctionName.getsEnumerationValue(records.get(i)[0].split(";")[1]),
+                        Integer.parseInt(records.get(i)[0].split(";")[2])));
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * Checks if a CSV file has the correct format for input.
+     * It verifies that the first row contains only one element (number of partition),
+     * and all subsequent rows contain two elements (key | data).
+     *
+     * @param csvFile the input file that contains the data.
+     * @return true if the file format is correct, false otherwise.
+     */
+    public static boolean checkCSVinput(File csvFile) {
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            List<String[]> records = reader.readAll();
+
+            // the 1st row represents the input file format
+            if (records.get(0)[0].split(";").length != 1) {
+                return false;
+            }
+
+            // reads all data
+            for (int i = 1; i < records.size(); i++) {
+                if (records.get(i)[0].split(";").length != 2) {
+                    return false;
+                }
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks if a CSV file has the correct format for input.
+     * It verifies that all rows contain three elements (operation | function | quantity).
+     *
+     * @param csvFile the input file that contains the data.
+     * @return true if the file format is correct, false otherwise.
+     */
+    public static boolean checkCSVoperator(File csvFile) {
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            List<String[]> records = reader.readAll();
+
+            // reads all operation
+            for (String[] record : records) {
+                if (record[0].split(";").length != 3) {
+                    return false;
+                }
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Method takes a list of CSV files as input and concatenates their contents
+     * into a single file.
+     *
+     * @param paths the list of all input data to concatenate.
+     * @return a concatenate file.
+     */
+    public static File concatenateCSVfile(List<String> paths) {
+        StringBuilder completeData = new StringBuilder();
+
+        for (int i = 0; i < paths.size(); i++) {
+            try {
+                completeData.append(Files.readString(Paths.get(paths.get(i))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        File outputFile = new File(paths.get(0));
+
+        try {
+            outputFile.createNewFile();
+            FileWriter writer = new FileWriter(outputFile);
+            writer.write(completeData.toString());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return outputFile;
+    }
+}
+
+/*
+ * CSV INPUT file format:
+ * number of partition
+ * key | value
+ * key | value
+ * ...
+ *
+ * CSV OPERATION file format:
+ * Operation | Function | Quantity
+ */
---
+++ src/main/java/it/polimi/ds/function/Function.java
@@ -0,0 +1,165 @@
+package it.polimi.ds.function;
+
+import static java.lang.Math.abs;
+
+
+/**
+ * The Function class provides a set of static methods that perform basic mathematical operations such as addition, subtraction, multiplication, and division.
+ */
+public class Function {
+    /**
+     * Add function.
+     *
+     * @param number the input number.
+     * @param adder the quantity to add from the number.
+     * @return number + adder.
+     */
+    private static int add(int number, int adder) {
+        return (number + adder);
+    }
+
+    /**
+     * Subtract function.
+     *
+     * @param number the input number.
+     * @param subtract the quantity to subtract from the number.
+     * @return number - subtract.
+     */
+    private static int sub(int number, int subtract) {
+        return (number - subtract);
+    }
+
+    /**
+     * Multiply function.
+     *
+     * @param number the input number.
+     * @param multiplier the quantity to multiply from the number.
+     * @return number * multiplier.
+     */
+    private static int mult(int number, int multiplier) {
+        return (number * multiplier);
+    }
+
+    /**
+     * Division function.
+     *
+     * @param number the input number.
+     * @param divider the quantity to divide from the number.
+     * @return number / divider.
+     */
+    private static int div(int number, int divider) {
+        return (number / divider);
+    }
+
+    /**
+     * Compares the number with the equal value.
+     *
+     * @param number the input number.
+     * @param equal the equality quantity.
+     * @return 1 if the number is equal, 0 otherwise.
+     */
+    private static int equal(int number, int equal) {
+        return number == equal ? 1 : 0;
+    }
+
+    /**
+     * Compares the number with the notEqual value.
+     *
+     * @param number the input number.
+     * @param notEqual the not equality quantity.
+     * @return 1 if the number is not equal, 0 otherwise.
+     */
+    private static int notEqual(int number, int notEqual) {
+        return number != notEqual ? 1 : 0;
+    }
+
+    /**
+     * Compares the number with the greater value.
+     *
+     * @param number the input number.
+     * @param greater the greater quantity.
+     * @return 1 if the number is greater, 0 otherwise.
+     */
+    private static int greaterThan(int number, int greater) {
+        return number > greater ? 1 : 0;
+    }
+
+    /**
+     * Compares the number with the greaterEqual value.
+     *
+     * @param number the input number.
+     * @param greaterEqual the greater or equal quantity.
+     * @return 1 if the number is greater or equal, 0 otherwise.
+     */
+    private static int greaterEqual(int number, int greaterEqual) {
+        return number >= greaterEqual ? 1 : 0;
+    }
+
+    /**
+     * Compares the number with the lower value.
+     *
+     * @param number the input number.
+     * @param lower the lower quantity.
+     * @return 1 if the number is lower, 0 otherwise.
+     */
+    private static int lowerThan(int number, int lower) {
+        return number < lower ? 1 : 0;
+    }
+
+    /**
+     * Compares the number with the lowerEqual value.
+     *
+     * @param number the input number.
+     * @param lowerEqual the lower or equal quantity.
+     * @return 1 if the number is lower or equal, 0 otherwise.
+     */
+    private static int lowerEqual(int number, int lowerEqual) {
+        return number <= lowerEqual ? 1 : 0;
+    }
+
+
+    /**
+     * Selects and executes one of the predefined functions.
+     *
+     * @param number the input number.
+     * @param quantity the quantity to add/subtract/multiply/divide from the number.
+     * @param type the default function type to call.
+     * @return result = f(number).
+     */
+    public static int f(int number, int quantity, FunctionName type) {
+        switch (type) {
+            case ADD -> {
+                return add(number, quantity);
+            }
+            case SUB -> {
+                return sub(number, quantity);
+            }
+            case MULT -> {
+                return mult(number, quantity);
+            }
+            case DIV -> {
+                return div(number, quantity);
+            }
+            case EQUAL -> {
+                return equal(number, quantity);
+            }
+            case NOT_EQUAL -> {
+                return notEqual(number, quantity);
+            }
+            case GREATER_THAN -> {
+                return greaterThan(number, quantity);
+            }
+            case GREATER_OR_EQUAL -> {
+                return greaterEqual(number, quantity);
+            }
+            case LOWER_THAN -> {
+                return lowerThan(number, quantity);
+            }
+            case LOWER_OR_EQUAL -> {
+                return lowerEqual(number, quantity);
+            }
+        }
+
+        return -1;
+    }
+}
