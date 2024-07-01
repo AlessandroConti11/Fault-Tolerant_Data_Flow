dcc9cadb40b89e9a0256180ffe9cc315fc104a0b
sbancuz <sbancuz@protonmail.com>
2024-07-01 11:05:08 +0200

    Pretty colors YAYYYY!!

--- src/main/java/it/polimi/ds/Allocator.java
+++ src/main/java/it/polimi/ds/Allocator.java
@@ -47,7 +47,8 @@ public class Allocator {
                                 .setAddress(coord_addr.toProto())
                                 .build());
 
-                        spoofOutput(proc, "[COORDINATOR(" + procId + ")] ");
+                        spoofOutput(proc,
+                                "[" + colors[procId % colors.length] + "COORDINATOR(" + procId + ")" + RESET + "] ");
                     } else if (req.hasNodeManagerInfo()) {
                         NodeManagerInfo info = req.getNodeManagerInfo();
                         for (int i = 0; i < info.getNumContainers(); i++) {
@@ -57,7 +58,8 @@ public class Allocator {
                                     .redirectOutput(ProcessBuilder.Redirect.PIPE)
                                     .start();
 
-                            spoofOutput(proc, "[WORKER(" + procId + ")] ");
+                            spoofOutput(proc,
+                                    "[" + colors[procId % colors.length] + "WORKER(" + procId + ")" + RESET + "] ");
                         }
 
                         conn.send(AllocateNodeManagerResponse.newBuilder().build());
@@ -88,4 +90,18 @@ public class Allocator {
             }
         }).start();
     }
+
+    public static final String RESET = "\033[0m"; // Text Reset
+    public static final String BLACK = "\033[0;30m"; // BLACK
+    public static final String RED = "\033[0;31m"; // RED
+    public static final String GREEN = "\033[0;32m"; // GREEN
+    public static final String YELLOW = "\033[0;33m"; // YELLOW
+    public static final String BLUE = "\033[0;34m"; // BLUE
+    public static final String PURPLE = "\033[0;35m"; // PURPLE
+    public static final String CYAN = "\033[0;36m"; // CYAN
+    public static final String WHITE = "\033[0;37m"; // WHITE
+
+    private static final String[] colors = {
+            BLACK, RED, GREEN, YELLOW, BLUE, PURPLE, CYAN, WHITE
+    };
 }
